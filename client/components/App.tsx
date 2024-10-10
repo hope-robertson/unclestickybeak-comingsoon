43a19c435279eac2831ab4f82c8a985b5c3a0495
Hope Robertson <hope.robertson.nz@gmail.com>
2024-10-11 12:56:30 +1300

    created coming soon component to display image

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,9 @@
+import ComingSoon from './ComingSoon'
+
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <ComingSoon />
     </div>
   )
 }
---
+++ client/components/ComingSoon.tsx
@@ -0,0 +1,11 @@
+// ComingSoon.tsx (client/components/ComingSoon.tsx)
+
+function ComingSoon() {
+  return (
+    <div className="comingSoonImageContainer">
+      <img width="577" src="./assets/coming_soon.png" alt="Coming Soon" />
+    </div>
+  )
+}
+
+export default ComingSoon
